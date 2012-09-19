723312414e695fea034ab788c04082f19a256f55
Aristeides Stathopoulos <aristath@gmail.com>
2012-09-19 19:09:55 +0200

    when viewing a category, list it's subcategories

--- mp_category.php
+++ mp_category.php
@@ -5,6 +5,11 @@
 	<div id="main" class="<?php echo MAIN_CLASSES; ?>" role="main">
 		<?php if ( class_exists( 'MarketPress' ) ) { ?>
 			<div id="product-grid">
+				<ul class="subcategories">
+					<?php $term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+					wp_list_categories('taxonomy=product_category&depth=0&show_count=0&&title_li=&child_of=' . $term->term_id . '&show_option_none=');
+					?>
+				</ul>
 				<div class="clearfix"></div>
 				<?php freemarket_list_products();?>
 			</div>
