2a80025a0f7f82e0cf433fc5e6974f4faf6886a9
Aristeides Stathopoulos <aristath@gmail.com>
2012-10-03 20:49:23 +0200

    finally managed to prepopulate menu items

--- includes/menus.php
+++ includes/menus.php
@@ -1,4 +1,31 @@
 <?php
 
-wp_create_nav_menu( 'FreeMarket Top Menu', array( 'slug' => 'freemarket-top-menu' ) );
-wp_create_nav_menu( 'FreeMarket Main Menu', array( 'slug' => 'freemarket-main-menu' ) );
+add_action( 'init', 'freemarket_register_navmenus' );
+function freemarket_register_navmenus() {
+  register_nav_menus( array(
+    'freemarket-top-menu'     => __( 'FreeMarket Top Navigation', 'freemarket' ),
+    'freemarket-main-menu'    => __( 'FreeMarket Main Navigation', 'freemarket' ),
+    )
+  );
+
+  // Check if Top menu exists and make it if not
+  if ( !is_nav_menu( 'freemarket-top-menu' )) {
+    $menu_id = wp_create_nav_menu( 'freemarket-top-menu' );
+    $menu = array( 'menu-item-type' => 'custom', 'menu-item-url' => get_home_url(),'menu-item-title' => 'Home', 'menu-item-status' => 'publish' );
+    wp_update_nav_menu_item( $menu_id, 0, $menu );
+
+    set_theme_mod( 'nav_menu_locations', array(
+      'freemarket-top-menu' => $menu_id,
+    ) );
+  }
+  // Check if Header menu exists and make it if not
+  if ( !is_nav_menu( 'freemarket-main-menu' )) {
+    $menu_id = wp_create_nav_menu( 'freemarket-main-menu' );
+    $menu = array( 'menu-item-type' => 'custom', 'menu-item-url' => get_home_url(), 'menu-item-title' => 'Home', 'menu-item-status' => 'publish' );
+    wp_update_nav_menu_item( $menu_id, 0, $menu );
+
+    set_theme_mod( 'nav_menu_locations', array(
+      'freemarket-main-menu' => $menu_id,
+    ) );
+  }
+}
